3a8d9364cd81e891e8b061b7d5f482de4df6834d
unknown <angelamarcelop@gmail.com>
2025-06-04 01:40:06 -0500

    modificando A BD ATLAS

--- ecommerce-backend/app.js
+++ ecommerce-backend/app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Conectado a MongoDB correctamente'))
   .catch((error) => console.error('Error de conexión:', error.message));
 
